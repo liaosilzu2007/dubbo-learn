5976c4f95e5d835602ef6e494c41a5605be57612
liaosi <liaosilzu2007@163.com>
2017-10-08 23:04:07 +0800

    #

--- demo-provider/src/main/java/com/ddcx/dubbolearn/demo/Launcher.java
+++ demo-provider/src/main/java/com/ddcx/dubbolearn/demo/Launcher.java
@@ -4,6 +4,8 @@ import com.ddcx.dubbolearn.demo.util.BeanFactoryUtil;
 import com.ddcx.dubbolearn.demo.util.SystemDetails;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.net.InetAddress;
 import java.util.function.Consumer;
@@ -29,7 +31,8 @@ public class Launcher {
         getLocalip();
         // 初始化spring
         log.info("开始初始化core服务");
-        BeanFactoryUtil.init();
+        ClassPathXmlApplicationContext applicationContext = (ClassPathXmlApplicationContext) BeanFactoryUtil.getContext();
+        applicationContext.start();
 
         try{
             System.in.read();
